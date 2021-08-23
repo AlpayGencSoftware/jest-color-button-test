0e1381bca4c7dcf979edf4cd4687f0ee11d3e090
AlpayGencSoftware <gencoalpay@gmail.com>
2021-08-23 13:18:56 +0300

    Test and code gray background for disabled button

--- src/App.test.js
+++ src/App.test.js
@@ -55,8 +55,44 @@ test('Checkbox disables button on first click and enables on second click',() =>
     expect(colorButton).toBeEnabled();
 });
 
-test('button turns blue when clicked', ()=>{
+ 
+
+test('button turns grey background and reverts to red', ()=>{
     render(<App/>);
-    const colorButton= screen.getByRole('button', {name :'Change to blue'});
+    const checkbox = screen.getByRole('checkbox', {name: 'Disable button'});
+    const colorButton= screen.getByRole('button',{name :'Change to blue'});
+
+    fireEvent.click(colorButton);
+    fireEvent.click(checkbox);
+    expect(colorButton).toHaveStyle('background-color:gray');
+    fireEvent.click(checkbox);
+    expect(colorButton).toHaveStyle('background-color:red');
+
 });
+test('Clicked disabled button has gray background and reverts to red', ()=>{
+    render(<App/>);
+    const checkbox = screen.getByRole('checkbox', {name: 'Disable button'});
+    const colorButton = screen.getByRole('button', {name: 'Change to red'});
+
+    fireEvent.click(colorButton);
+    fireEvent.click(checkbox);
+    expect(colorButton).toHaveStyle('background-color:gray');
+    fireEvent.click(checkbox);
+    expect(colorButton).toHaveStyle('background-color:red');
+});
+
+test('Clicked disabled button has gray background and reverts to blue', ()=>{
+    render(<App/>);
+    const checkbox = screen.getByRole('checkbox', {name: 'Disable button'});
+    const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+
+    fireEvent.click(colorButton);
+    fireEvent.click(checkbox);
+    expect(colorButton).toHaveStyle('background-color:gray');
+    fireEvent.click(checkbox);
+    expect(colorButton).toHaveStyle('background-color:blue');
+});
+
+
+
  
